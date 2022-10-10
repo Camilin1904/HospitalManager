decef16208d59898f5e91efb0be504edad098963
MateoSilvaLasso <1000035385@u.icesi.edu.co>
2022-10-10 10:15:48 -0500

    priority implement

---
+++ src/model/Heap.java
@@ -0,0 +1,14 @@
+package src.model;
+
+public interface Heap<V> {
+
+    public void heapify (int i);
+
+    public void buildMaxHeap();
+
+    public void heapSort();
+    
+    
+
+
+}
---
+++ src/model/PriorityQueue.java
@@ -0,0 +1,14 @@
+package src.model;
+
+public interface PriorityQueue<T>{
+
+    public T heapMaximum();
+
+    public T heapExtractMax();
+
+    public void increaseKey(int i, int key);
+
+    public void insert(T element, int key);
+
+    
+} 
